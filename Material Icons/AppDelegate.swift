54510c544b7b7ea6ee4527015f86fd989b5a3366
Keaton Burleson <keaton.burleson@me.com>
2020-09-17 09:54:04 -0500

    Initial Commit

---
+++ Material Icons/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Material Icons
+//
+//  Created by Keaton Burleson on 9/17/20.
+//  Copyright © 2020 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Material Icons/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Material Icons
+//
+//  Created by Keaton Burleson on 9/17/20.
+//  Copyright © 2020 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
